ce371dbae379c75ff8640621c4ef6ae370b5d836
jonathanstokes <jonsto@gmail.com>
2012-03-19 23:03:28 -0400

    Initial Commit

---
+++ whiskers/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  whiskers
+//
+//  Created by Jon Stokes on 3/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ whiskers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  whiskers
+//
+//  Created by Jon Stokes on 3/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
